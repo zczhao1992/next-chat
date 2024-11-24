d1aaecd103c62a2f8bc866c4fb95a75a2f4cdc15
zczhao1992 <zczhao1992@163.com>
2024-11-24 18:23:50 +0800

    fix:clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
